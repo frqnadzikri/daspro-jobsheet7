983b1bc86d2df1e20f4e286885784d0d42561577
Furqon Adzikri <furqonadzikri311@gmail.com>
2025-10-23 22:59:44 +0700

    modifikasi percobaan 1

--- SiakadFor15.java
+++ SiakadFor15.java
@@ -7,6 +7,7 @@ public class SiakadFor15 {
         
         // Deklarasi variabel
         double nilai, tertinggi = 0, terendah = 100;
+        int lulus = 0, tidakLulus = 0;
         
         // Perulangan untuk 10 mahasiswa
         for (int i = 1; i <= 10; i++) {
@@ -23,14 +24,23 @@ public class SiakadFor15 {
             if (nilai < terendah) {
                 terendah = nilai;
             }
+            
+            // Hitung mahasiswa lulus dan tidak lulus (batas kelulusan 60)
+            if (nilai >= 60) {
+                lulus++;
+            } else {
+                tidakLulus++;
+            }
         }
         
         // Tampilkan hasil
+        System.out.println("\n=== HASIL PERHITUNGAN ===");
         System.out.println("Nilai tertinggi: " + tertinggi);
         System.out.println("Nilai terendah: " + terendah);
+        System.out.println("Jumlah mahasiswa lulus: " + lulus);
+        System.out.println("Jumlah mahasiswa tidak lulus: " + tidakLulus);
         
+        // Tutup scanner
         sc.close();
     }
 }
-    
-
