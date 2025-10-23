d83d8f50f9fc044fecfd415638d6a1b691d3dfc2
Furqon Adzikri <furqonadzikri311@gmail.com>
2025-10-23 23:38:15 +0700

    Modifikasi Percobaan 2

--- SiakadWhile15.java
+++ SiakadWhile15.java
@@ -3,7 +3,6 @@ import java.util.Scanner;
 public class SiakadWhile15 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        
         int nilai, jml, i = 0;
         
         // Input jumlah mahasiswa
@@ -24,6 +23,7 @@ public class SiakadWhile15 {
             // Menentukan kategori huruf
             if (nilai > 80 && nilai <= 100) {
                 System.out.println("Nilai mahasiswa ke-" + (i + 1) + " adalah A");
+                System.out.println("Bagus, pertahankan nilainya!");
             } else if (nilai > 73 && nilai <= 80) {
                 System.out.println("Nilai mahasiswa ke-" + (i + 1) + " adalah B+");
             } else if (nilai > 65 && nilai <= 73) {
@@ -41,8 +41,7 @@ public class SiakadWhile15 {
             i++;
         }
         
+        // Tutup scanner
         sc.close();
     }
-} 
-    
-
+}
